06b837e1540f91ad97ffeb75d96b30e87e8db726
zhidkov <nikita@zhidkovers.ru>
2026-07-26 14:56:33 +0400

    Fixed links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://anomalyco.github.io',
+  site: 'https://zhidkovers.github.io',
   base: '/opentgl/',
   outDir: 'dist',
   publicDir: 'public',
